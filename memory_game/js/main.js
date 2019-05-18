9049f12c9d8c35c4f644954be4d2e491d526d3c6
Kevinn Arbas <kevinnarbas@Angies-MacBook-Air.local>
2019-05-18 15:50:16 -0500

    added flipping card

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,25 +25,39 @@ var cards = [
 
 var cardsInPlay = []
 
-function checkForMatch(){
-    if (cardsInPlay.length === 2) {
-        if (cardsInPlay[0] === cardsInPlay[1]){
-        alert("You found a match!");
-        }
-        else {
-        alert("Sorry Try Again")
-        }
-    }
+function createBoard(){
+    for(i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    };
 }
 
-function flipCard(cardId){
+
+function flipCard(){
+    var cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank);
     console.log(cards[cardId].suit);
     console.log(cards[cardId].cardImage);
-    cardsInPlay.push(cards[cardId].suit);    
-    checkForMatch();
+    cardsInPlay.push(cards[cardId].rank); 
+    this.setAttribute('src', cards[cardId].cardImage)   
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }    
 }
 
-flipCard(0);
-flipCard(2);
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+    }
+    else {
+    alert("Sorry Try Again")
+    }
+    
+    
+}
 
+
+createBoard();
